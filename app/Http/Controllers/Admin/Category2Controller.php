19d7e0b19606b7bed76f4c2e67f140866f9acd5c
vipos89 <1989146ViPoS>
2022-12-12 14:13:32 +0300

    add controller

---
+++ app/Http/Controllers/Admin/Category2Controller.php
@@ -0,0 +1,80 @@
+<?php
+
+    namespace App\Http\Controllers\Admin;
+
+    use App\Http\Controllers\Controller;
+    use App\Models\Category;
+    use App\Models\User;
+    use Illuminate\Http\Request;
+
+    class Category2Controller extends Controller
+    {
+        /**
+         * Display a listing of the resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function index()
+        {
+            ddd(Category::all());
+
+            return view('admin.categories.index', [
+                'categories' => Category::paginate(5),
+            ]);
+        }
+
+        /**
+         * Show the form for creating a new resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function create()
+        {
+            return view('admin.categories.create');
+        }
+
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param \Illuminate\Http\Request $request
+         * @return \Illuminate\Http\Response
+         */
+        public function store(Request $request)
+        {
+        }
+
+
+        /**
+         * Show the form for editing the specified resource.
+         *
+         * @param \App\Models\Category $category
+         * @return \Illuminate\Http\Response
+         */
+        public function edit(Category $category)
+        {
+            //
+        }
+
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param \Illuminate\Http\Request $request
+         * @param \App\Models\Category $category
+         * @return \Illuminate\Http\Response
+         */
+        public function update(Request $request, Category $category)
+        {
+            //
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param \App\Models\Category $category
+         * @return \Illuminate\Http\Response
+         */
+        public function destroy(Category $category)
+        {
+            //
+        }
+    }
